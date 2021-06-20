7191da57900d54ac37e039d7639d2226eb02bb46
gelwg1 <gelwg1@gmail.com>
2021-06-20 19:43:34 +0700

    change css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import UserProvider, { UserContext, withAuth } from './context/UserContext'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './bootstrap.min.css';
 import './style.css';
 import App from './components/App'
 
